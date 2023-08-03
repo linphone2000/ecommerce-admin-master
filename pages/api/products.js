9a12c5a5e901c72cb36e38746459fa8bb9966abe
Lin Phone Myint Zaw <linphonem@gmail.com>
2023-08-03 08:49:46 +0630

    3/8/2023 8:45AM

---
+++ pages/api/products.js
@@ -0,0 +1,10 @@
+import clientPromise from "@/lib/mongodb";
+import mongoose from "mongoose";
+
+export default function handle(req,res){
+    const {method} = req;
+    mongoose.connect(clientPromise.url)
+    if(method === 'POST'){
+        res.json('post');
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,25 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-500 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">
-            Login with Google
-          </button>
+  const {data: session} = useSession();
+  if(!session) return;
+  console.log(session);
+  return(
+    <Layout>
+      <div className="text-cyan-800 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden">
+          <img src={session?.user?.image} className="w-6 h-6"/>
+          <span className="px-2">
+            {session?.user?.name}
+          </span>
         </div>
       </div>
-    );
-  }
-  return (
-    <div>Logged in with {session.user.email}</div>
-  );
+    </Layout>
+  )
 }
